713a5b812fc5f1e704f2e41525bd949caebae283
kkumtree <mscho7969@ubuntu.com>
2025-11-01 17:19:13 +0900

    add: 3w folder

---
+++ 3w/shells/kind/up-kind.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+kind version
+kind create cluster --name myk8s --image kindest/node:v1.32.8 --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+  apiServerAddress: "0.0.0.0"
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 30000
+    hostPort: 30000
+  - containerPort: 30001
+    hostPort: 30001
+  - containerPort: 30002
+    hostPort: 30002
+  - containerPort: 30003
+    hostPort: 30003
+- role: worker
+EOF
