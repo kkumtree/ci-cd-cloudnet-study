310040b4b87d0d84ed9ec1266c92ba7b05ea7d2a
kkumtree <mldd.kor@gmail.com>
2025-11-16 18:13:43 +0900

    add: 5w ingress basic

---
+++ 5w/shells/argocd/deploy-chart.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+kubectl create ns argocd
+
+# confirm cert and key is available in the path
+kubectl -n argocd create secret tls argocd-server-tls \
+  --cert=openssl-x509-output/argocd.example.com.crt \
+  --key=openssl-x509-output/argocd.example.com.key
+
+# https://github.com/argoproj/argo-helm/blob/main/charts/argo-cd/values.yaml
+
+cat <<EOF > argocd-values.yaml
+global:
+  domain: argocd.example.com
+
+# # TLS certificate configuration via cert-manager
+# # cert-manager가 있을 때, 활용.  
+# certificate: 
+#   enabled: true
+
+server:
+  ingress:
+    enabled: true
+    ingressClassName: nginx
+    annotations:
+      nginx.ingress.kubernetes.io/force-ssl-redirect: "true"
+      nginx.ingress.kubernetes.io/ssl-passthrough: "true"
+    tls: true
+EOF
+
+# https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd#installing-the-chart
+helm repo add argo https://argoproj.github.io/argo-helm
+# https://github.com/argoproj/argo-helm/releases
+helm install argocd argo/argo-cd --version 9.0.5 -f argocd-values.yaml --namespace argocd
---
+++ 5w/shells/kind/up-kind.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+kind version
+kind create cluster --name myk8s --image kindest/node:v1.32.8 --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+# networking:
+#   apiServerAddress: "0.0.0.0"
+nodes:
+- role: control-plane
+  labels:
+    ingress-ready: true
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+  - containerPort: 30000
+    hostPort: 30000
+  - containerPort: 30001
+    hostPort: 30001
+  - containerPort: 30002
+    hostPort: 30002
+  - containerPort: 30003
+    hostPort: 30003
+- role: worker
+EOF
